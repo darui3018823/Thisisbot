8127a443f1408fa706b1e5443d07bbef9daa0c4a
darui3018823 <151906210+darui3018823@users.noreply.github.com>
2024-11-08 01:59:52 +0900

    Add files via upload

---
+++ main.py-ver2.4.0/imports/preload.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+import json
+import sys
+import os
+
+# Read perm.json
+def load_permissions():
+    if not os.path.exists('perm.json'):
+        return {}
+    with open('perm.json', 'r', encoding='UTF-8') as f:
+        data = json.load(f)
+        return data
+    
+# Write perm.json
+def save_permissions(permissions):
+    with open('perm.json', 'w', encoding='UTF-8') as f:
+        json.dump(permissions, f, ensure_ascii=False, indent=4)
+        
+# Read blacklist.json
+def load_blacklist():
+    try:
+        with open('blacklist.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+# Write blacklist.json
+def save_blacklist(blacklist):
+    with open('blacklist.json', 'w') as f:
+        json.dump(blacklist, f, indent=4)
+
+# Read commands.json
+def load_commands_json():
+    if not os.path.exists('commands.json'):
+        return {}
+    with open('commands.json', 'r', encoding='UTF-8') as f:
+        return json.load(f)
+    
+# convからの移植
+def convert_link(original_url):
+    if original_url.startswith("https://x.com/") or original_url.startswith("https://twitter.com/"):
+        return original_url.replace("https://x.com/", "https://fxtwitter.com/").replace("https://twitter.com/", "https://fxtwitter.com/")
+    return original_url
+def log_conversion(user, original_url, converted_url):
+    try:
+        convtime = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+        with open("convlog.txt", "a") as log_file:
+            log_file.write(f"User: {user.name}, {user.id}\n")
+            log_file.write(f"Time at: {convtime}\n")
+            log_file.write(f"From: {original_url}\n")
+            log_file.write(f"To: {converted_url}\n\n")
+            print(f"User: {user.name}\nTime at: {convtime}\nFrom: {original_url}\nTo: {converted_url}")
+    except Exception as e:
+        print(f"An error occurred while logging conversion: {e}")
+
+
+def get_python_version():
+    # Pythonバージョンを取得
+    return sys.version
+def get_current_directory():
+    # カレントディレクトリを取得
+    return os.getcwd()
+def is_virtual_env():
+    # 仮想環境かどうかを確認
+    return sys.prefix != sys.base_prefix
